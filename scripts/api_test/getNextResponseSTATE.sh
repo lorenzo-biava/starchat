ac175f8632ff3fb2f69e8fb29ad4ed7b2b68feb8
Angelo Leto <angleto@gmail.com>
2017-03-06 22:00:31 +0100

    imported modifications

--- scripts/api_test/getNextResponseSTATE.sh
+++ scripts/api_test/getNextResponseSTATE.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl -v -H "Content-Type: application/json" -X POST http://localhost:8888/get_next_response -d '{
-	"user_id": "1234",
+	"conversation_id": "1234",
 	"user_input": { "text": "" },
 	"values": {
 		"return_value": "further_details_access_question",
