0a56f4bbcb7889da515b8db47a98441c8a1d57f6
Mario Alemi <mario.alemi@gmail.com>
2017-03-14 10:16:33 +0100

    small change on GET /

--- src/main/scala/com/getjenny/starchat/entities/RootAPIsDescription.scala
+++ src/main/scala/com/getjenny/starchat/entities/RootAPIsDescription.scala
@@ -6,7 +6,7 @@ package com.getjenny.starchat.entities
 
 /*parameters*/
 case class RootAPIsDescription(api : Map[String, String] = Map(
-  "/" -> "this help, see https://github.com/GetJenny/starchat",
+  "/" -> "Welcome! For help, see https://github.com/GetJenny/starchat",
   "/get_next_response" -> "supported methods: POST",
   "/decisiontable" -> "supported methods: GET, PUT, POST, DELETE",
   "/decisiontable_search" -> "supported methods: POST",
