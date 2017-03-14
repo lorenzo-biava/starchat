250cec14da25598af227a8db9ac2b041aea5cc81
Angelo Leto <leto@celi.it>
2017-03-14 22:44:48 +0100

    added search: from text to terms

--- src/main/scala/com/getjenny/starchat/services/TermService.scala
+++ src/main/scala/com/getjenny/starchat/services/TermService.scala
@@ -14,9 +14,12 @@ import scala.collection.immutable.{List, Map}
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.xcontent.XContentFactory._
 import org.elasticsearch.action.get.{GetResponse, MultiGetItemResponse, MultiGetRequestBuilder, MultiGetResponse}
+import org.elasticsearch.action.search.{SearchRequestBuilder, SearchResponse, SearchType}
+import org.elasticsearch.index.query.{BoolQueryBuilder, QueryBuilders}
 
 import scala.collection.JavaConverters._
 import org.elasticsearch.rest.RestStatus
+import org.elasticsearch.search.SearchHit
 
 /**
   * Implements functions, eventually used by TermResource
@@ -25,7 +28,7 @@ class TermService(implicit val executionContext: ExecutionContext) {
   val elastic_client = IndexManagementClient
 
   def vector2string(vector: Vector[Double]): String = {
-    vector.zipWithIndex.map(x => (x._2.toString + "|" + x._1.toString)).mkString(" ")
+    vector.zipWithIndex.map(x => x._2.toString + "|" + x._1.toString).mkString(" ")
   }
 
   def index_term(terms: Terms) : Option[IndexDocumentListResult] = {
@@ -49,10 +52,9 @@ class TermService(implicit val executionContext: ExecutionContext) {
         case None => ;
       }
       term.vector match {
-        case Some(t) => {
+        case Some(t) =>
           val indexable_vector: String = vector2string(t)
           builder.field("vector", indexable_vector)
-        }
         case None => ;
       }
       builder.endObject()
@@ -107,7 +109,7 @@ class TermService(implicit val executionContext: ExecutionContext) {
           case None => None: Option[String]
       }
 
-      Term(term = term,
+      Term(term = term, //TODO: to complete
         synonyms = Option{Map.empty[String, Double]},
         antonyms = Option{Map.empty[String, Double]},
         frequency = frequency,
@@ -139,10 +141,9 @@ class TermService(implicit val executionContext: ExecutionContext) {
         case None => ;
       }
       term.vector match {
-        case Some(t) => {
+        case Some(t) =>
           val indexable_vector: String = vector2string(t)
           builder.field("vector", indexable_vector)
-        }
         case None => ;
       }
       builder.endObject()
@@ -191,8 +192,145 @@ class TermService(implicit val executionContext: ExecutionContext) {
   def search_term(term: Term) : Option[TermsResults] = {
     val client: TransportClient = elastic_client.get_client()
 
+    val search_builder : SearchRequestBuilder = client.prepareSearch(elastic_client.index_name)
+      .setTypes(elastic_client.term_type_name)
+      .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+
+    val bool_query_builder : BoolQueryBuilder = QueryBuilders.boolQuery()
+    bool_query_builder.must(QueryBuilders.termQuery("term.base", term.term))
+
+    if (term.frequency.isDefined)
+      bool_query_builder.must(QueryBuilders.termQuery("frequency", term.frequency.get))
+
+    if (term.vector.isDefined) {
+      //TODO: to complete
+      //bool_query_builder.must(QueryBuilders.termQuery("verified", documentSearch.verified.get))
+    }
+
+    if (term.synonyms.isDefined)
+      bool_query_builder.must(QueryBuilders.termQuery("synonyms", term.synonyms.get))
+
+    if (term.antonyms.isDefined)
+      bool_query_builder.must(QueryBuilders.termQuery("antonyms", term.antonyms.get))
+
+    search_builder.setQuery(bool_query_builder)
+
+    val search_response : SearchResponse = search_builder
+      .execute()
+      .actionGet()
+
+    val documents : List[Term] = search_response.getHits.getHits.toList.map({ case(e) =>
+      val item: SearchHit = e
+
+      val source : Map[String, Any] = item.getSource.asScala.toMap
+
+      val term : String = source.get("term").asInstanceOf[String]
+
+      val synonyms: Option[String] = source.get("synonyms") match {
+        case Some(t) => Option{t.asInstanceOf[String]}
+        case None => None: Option[String]
+      }
+
+      val antonyms : Option[String] = source.get("antonyms") match {
+        case Some(t) => Option {t.asInstanceOf[String]}
+        case None => None: Option[String]
+      }
+
+      val frequency : Option[Double] = source.get("antonyms") match {
+        case Some(t) => Option {t.asInstanceOf[Double]}
+        case None => None: Option[Double]
+      }
+
+      val vector : Option[String] = source.get("antonyms") match {
+        case Some(t) => Option {t.asInstanceOf[String]}
+        case None => None: Option[String]
+      }
+
+      Term(term = term, //TODO: to complete
+        synonyms = Option{Map.empty[String, Double]},
+        antonyms = Option{Map.empty[String, Double]},
+        frequency = frequency,
+        vector = None: Option[Vector[Double]],
+        score = Option{item.getScore.toDouble})
+    })
+
+    val terms: Terms = Terms(terms=documents)
+
+    val max_score : Float = search_response.getHits.getMaxScore
+    val total : Int = terms.terms.length
+    val search_results : TermsResults = TermsResults(total = total, max_score = max_score,
+      hits = terms)
+
     Option {
-      null
+      search_results
     }
   }
+
+  //given a text, return all terms which match
+  def search(text: String) : Option[TermsResults] = {
+    val client: TransportClient = elastic_client.get_client()
+
+    val search_builder : SearchRequestBuilder = client.prepareSearch(elastic_client.index_name)
+      .setTypes(elastic_client.term_type_name)
+      .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+
+    val bool_query_builder : BoolQueryBuilder = QueryBuilders.boolQuery()
+    bool_query_builder.must(QueryBuilders.termQuery("term.base", text))
+    search_builder.setQuery(bool_query_builder)
+
+    val search_response : SearchResponse = search_builder
+      .execute()
+      .actionGet()
+
+    val documents : List[Term] = search_response.getHits.getHits.toList.map({ case(e) =>
+      val item: SearchHit = e
+
+      val source : Map[String, Any] = item.getSource.asScala.toMap
+
+      val term : String = source.get("term").asInstanceOf[String]
+
+      val synonyms: Option[String] = source.get("synonyms") match {
+        case Some(t) => Option{t.asInstanceOf[String]}
+        case None => None: Option[String]
+      }
+
+      val antonyms : Option[String] = source.get("antonyms") match {
+        case Some(t) => Option {t.asInstanceOf[String]}
+        case None => None: Option[String]
+      }
+
+      val frequency : Option[Double] = source.get("antonyms") match {
+        case Some(t) => Option {t.asInstanceOf[Double]}
+        case None => None: Option[Double]
+      }
+
+      val vector : Option[String] = source.get("antonyms") match {
+        case Some(t) => Option {t.asInstanceOf[String]}
+        case None => None: Option[String]
+      }
+
+      Term(term = term, //TODO: to complete
+        synonyms = Option{Map.empty[String, Double]},
+        antonyms = Option{Map.empty[String, Double]},
+        frequency = frequency,
+        vector = None: Option[Vector[Double]],
+        score = Option{item.getScore.toDouble})
+    })
+
+    val terms: Terms = Terms(terms=documents)
+
+    val max_score : Float = search_response.getHits.getMaxScore
+    val total : Int = terms.terms.length
+    val search_results : TermsResults = TermsResults(total = total, max_score = max_score,
+      hits = terms)
+
+    Option {
+      search_results
+    }
+  }
+
 }
+
+
+
+
