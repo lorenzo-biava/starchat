0f572d3f600980d438bcf50bd46ec352fc035429
Mario Alemi <mario.alemi@gmail.com>
2017-03-15 14:49:31 +0100

    added Not operator & changed names

--- src/main/scala/com/getjenny/analyzer/operators/BooleanAndOperator.scala
+++ src/main/scala/com/getjenny/analyzer/operators/BooleanAndOperator.scala
@@ -6,13 +6,13 @@ import com.getjenny.analyzer.expressions._
   * Created by mal on 21/02/2017.
   */
 
-class AndOperator(children: List[Expression]) extends AbstractOperator(children: List[Expression]) {
-  override def toString: String = "boolean-and(" + children.mkString(", ") + ")"
+class BooleanAndOperator(children: List[Expression]) extends AbstractOperator(children: List[Expression]) {
+  override def toString: String = "booleanAnd(" + children.mkString(", ") + ")"
   def add(e: Expression, level: Int = 0): AbstractOperator = {
-    if (level == 0) new AndOperator(e :: children)
+    if (level == 0) new BooleanAndOperator(e :: children)
     else children.head match {
-      case c: AbstractOperator => new AndOperator(c.add(e, level - 1) :: children.tail)
-      case _ => throw OperatorException("boolean-and: trying to add to smt else than an operator")
+      case c: AbstractOperator => new BooleanAndOperator(c.add(e, level - 1) :: children.tail)
+      case _ => throw OperatorException("booleanAnd: trying to add to smt else than an operator")
     }
   }
   def evaluate(query: String): Double = {
--- src/main/scala/com/getjenny/analyzer/operators/DefaultFactoryOperator.scala
+++ src/main/scala/com/getjenny/analyzer/operators/DefaultFactoryOperator.scala
@@ -9,11 +9,13 @@ import com.getjenny.analyzer.interfaces._
 
 class DefaultFactoryOperator extends Factory[List[Expression], AbstractOperator] {
 
-  override val operations = Set("or" , "and", "conjunction", "disjunction", "bor", "band", "booleanor", "booleanand")
+  override val operations = Set("or" , "and", "conjunction", "disjunction", "bor", "band", "booleanor", "booleanand",
+    "booleanOr", "booleanAnd", "booleanNot", "booleannot", "bnot")
 
   override def get(name: String, argument: List[Expression]): AbstractOperator = name.filter(c => !c.isWhitespace ) match {
-    case ("booleanor" | "bor") => new OrOperator(argument)
-    case ("booleanand"| "band") => new AndOperator(argument)
+    case ("booleanOr" | "booleanor" | "bor") => new BooleanOrOperator(argument)
+    case ("booleanAnd"| "booleanand"| "band") => new BooleanAndOperator(argument)
+    case ("booleanNot"| "booleannot"| "bnot") => new BooleanNotOperator(argument)
     case ("conjunction" | "and") => new ConjunctionOperator(argument)
     case ("disjunction" | "or") => new DisjunctionOperator(argument)
     case _ => throw OperatorNotFoundException("Operator \'" + name + "\' not found")
