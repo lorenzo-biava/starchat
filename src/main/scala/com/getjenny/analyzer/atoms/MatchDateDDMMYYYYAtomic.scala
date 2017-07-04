2730ecd9795b4b4c685cd39620ef695b90530d0c
Angelo Leto <angelo@getjenny.com>
2017-07-04 12:08:19 +0200

    improved regex

--- src/main/scala/com/getjenny/analyzer/atoms/MatchDateDDMMYYYYAtomic.scala
+++ src/main/scala/com/getjenny/analyzer/atoms/MatchDateDDMMYYYYAtomic.scala
@@ -11,7 +11,7 @@ class MatchDateDDMMYYYYAtomic(val prefix: String) extends AbstractAtomic {
   override def toString: String = "matchDateDDMMYYYY(" + prefix + ")"
   val isEvaluateNormalized: Boolean = true
   val regex = """[""" + prefix + """day,""" + prefix + """month,""" + prefix + """year]""" +
-    """(?:(0[1-9]|[12][0-9]|3[01])(?:[- \/\.])(0[1-9]|1[012])(?:[- \/\.])((?:19|20)\d\d))"""
+    """(?:(0[1-9]|[12][0-9]|3[01])(?:[- \/\.])(0[1-9]|1[012])(?:[- \/\.])((?:19|20)\d\d)(?:[^0-9]+|$))"""
   val regex_extractor = new PatternExtractionRegex(regex)
 
   def evaluate(query: String): Result = {
