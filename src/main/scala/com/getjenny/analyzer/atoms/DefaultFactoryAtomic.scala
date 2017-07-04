9a49e3c63d4eff52b0dab15c097325a835250750
Angelo Leto <angelo@getjenny.com>
2017-07-04 12:34:25 +0200

    removed analyzers from default factory

--- src/main/scala/com/getjenny/analyzer/atoms/DefaultFactoryAtomic.scala
+++ src/main/scala/com/getjenny/analyzer/atoms/DefaultFactoryAtomic.scala
@@ -9,7 +9,6 @@ import com.getjenny.analyzer.interfaces._
 class DefaultFactoryAtomic extends Factory[String, AbstractAtomic] {
 
   override val operations = Set("keyword" , "similar", "synonym", "regex",
-    "similarCosEmd", "similarCosEmdState",
     "matchPatternRegex", "matchDateDDMMYYYY")
 
   override def get(name: String, argument: String): AbstractAtomic = name.filter(c => !c.isWhitespace ) match {
