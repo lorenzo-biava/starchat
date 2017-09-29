9613a2fdb9baddada7ec5dd263e74de4dd3dc921
Angelo Leto <angelo@getjenny.com>
2017-09-29 16:57:20 +0200

    reduced timeout

--- src/main/scala/com/getjenny/starchat/analyzer/atoms/SearchAtomic.scala
+++ src/main/scala/com/getjenny/starchat/analyzer/atoms/SearchAtomic.scala
@@ -39,7 +39,7 @@ class SearchAtomic(arguments: List[String]) extends AbstractAtomic {
 
     val state: Future[Option[SearchDTDocumentsResults]] = decisionTableService.search(dtDocumentSearch)
     //search the state with the closest query value, then return that state
-    val res : Option[SearchDTDocumentsResults] = Await.result(state, 60.seconds)
+    val res : Option[SearchDTDocumentsResults] = Await.result(state, 30.seconds)
 
     val res_count = if (res.isEmpty) 0 else res.get.total
 
