bf161649a123dc968fdfe755bd2377458fa93b43
Angelo Leto <angleto@gmail.com>
2018-06-14 20:00:23 +0200

    restored fields name

--- src/main/scala/com/getjenny/starchat/services/esclient/ConversationLogsElasticClient.scala
+++ src/main/scala/com/getjenny/starchat/services/esclient/ConversationLogsElasticClient.scala
@@ -6,7 +6,7 @@ package com.getjenny.starchat.services.esclient
 
 object ConversationLogsElasticClient extends QuestionAnswerElasticClient {
   override val indexSuffix: String = convLogsIndexSuffix
-  override val indexMapping: String = "question"
+  override val indexMapping: String = "question_answer"
   override val dictSizeCacheMaxSize: Int = config.getInt("es.dictSizeCacheMaxSize")
   override val totalTermsCacheMaxSize: Int = config.getInt("es.totalTermsCacheMaxSize")
   override val countTermCacheMaxSize: Int = config.getInt("es.countTermCacheMaxSize")
