faaf7ea984f7b0296c9ec0d1f548b1d9b2ab4b0b
Lauri <lauri.koskinen2@metropolia.fi>
2019-02-26 14:53:23 +0200

    added unit tests for language_guesser

--- src/test/scala/com/getjenny/starchat/resources/LanguageGuesserResourceTest.scala
+++ src/test/scala/com/getjenny/starchat/resources/LanguageGuesserResourceTest.scala
@@ -1,5 +1,134 @@
 package com.getjenny.starchat.resources
 
-class LanguageGuesserResourceTest {
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.BasicHttpCredentials
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
+import akka.testkit._
+import com.getjenny.starchat.entities._
+import com.getjenny.starchat.serializers.JsonSupport
+import com.getjenny.starchat.utils.Index
+import org.scalatest.{Matchers, WordSpec}
+import scala.concurrent.duration._
+
+class LanguageGuesserResourceTest extends WordSpec with Matchers with ScalatestRouteTest with JsonSupport {
+implicit def default(implicit system: ActorSystem) = RouteTestTimeout(10.seconds.dilated(system))
+  val service = TestFixtures.service
+  val routes = service.routes
+
+  val testAdminCredentials = BasicHttpCredentials("admin", "adminp4ssw0rd")
+  val testUserCredentials = BasicHttpCredentials("test_user", "p4ssw0rd")
+
+  "StarChat" should {
+    "return an HTTP code 201 when creating a new system index" in {
+      Post(s"/system_index_management/create") ~> addCredentials(testAdminCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.Created
+        val response = responseAs[IndexManagementResponse]
+        response.message should fullyMatch regex "IndexCreation: " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.systemIndexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\) " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.systemIndexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\) " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.systemIndexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\) " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.systemIndexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\)".r
+      }
+    }
+  }
+
+  it should {
+    "return an HTTP code 201 when creating a new user" in {
+      val user = User(
+        id = "test_user",
+        password = "3c98bf19cb962ac4cd0227142b3495ab1be46534061919f792254b80c0f3e566f7819cae73bdc616af0ff555f7460ac96d88d56338d659ebd93e2be858ce1cf9",
+        salt = "salt",
+        permissions = Map[String, Set[Permissions.Value]]("index_getjenny_english_0" -> Set(Permissions.read, Permissions.write))
+      )
+      Post(s"/user", user) ~> addCredentials(testAdminCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.Created
+      }
+    }
+  }
+
+  it should {
+    "return an HTTP code 201 when creating a new index" in {
+      Post(s"/index_getjenny_english_0/index_management/create") ~> addCredentials(testAdminCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.Created
+        val response = responseAs[IndexManagementResponse]
+        response.message should fullyMatch regex "IndexCreation: " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.indexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\) " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.indexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\) " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.indexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\) " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.indexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\) " +
+          "(?:[A-Za-z0-9_]+)\\(" + Index.indexMatchRegex + "\\.(?:[A-Za-z0-9_]+), true\\)".r
+      }
+    }
+  }
+
+  val languages = List(
+    ("en", "guess the language of this sentence"),
+    ("fi", "arvaa tämän lauseen kieli"),
+    ("sv", "gissa språket i denna mening"),
+    ("et", "vist selle lause keelt"),
+    ("no", "gjett språket i denne setningen"),
+    ("it", "indovina la lingua di questa frase"),
+    ("ar", "تخمين لغة هذه الجملة"),
+    ("ru", "угадать язык этого предложения")
+  )
+
+  for((language, sentence) <- languages) {
+    it should {
+      s"return an HTTP code 200 when checking if '$language' is supported" in {
+        Get(s"/index_getjenny_english_0/language_guesser/$language") ~> addCredentials(testUserCredentials) ~> routes ~> check {
+          status shouldEqual StatusCodes.OK
+          val response = responseAs[LanguageGuesserInformations]
+          response.supportedLanguages should (contain key ("languages") and contain value (Map(language -> true)))
+        }
+      }
+    }
+
+    it should {
+      val languageGuesserRequestIn: LanguageGuesserRequestIn = LanguageGuesserRequestIn(
+        inputText = sentence
+      )
+
+      s"return an HTTP code 200 when guessing '$language' language" in {
+        Post(s"/index_getjenny_english_0/language_guesser", languageGuesserRequestIn) ~> addCredentials(testUserCredentials) ~> routes ~> check {
+          status shouldEqual StatusCodes.OK
+          val response = responseAs[LanguageGuesserRequestOut]
+          response.language should be(language)
+        }
+      }
+    }
+  }
+
+  it should {
+    val languageGuesserRequestIn: LanguageGuesserRequestIn = LanguageGuesserRequestIn(
+      inputText = "I am unauthorized"
+    )
+    val unauthorizedUserCredentials = BasicHttpCredentials("jack", "sparrow")
+
+    s"return an HTTP code 401 when unauthorized" in {
+      Post(s"/index_getjenny_english_0/language_guesser", languageGuesserRequestIn) ~> addCredentials(unauthorizedUserCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.Unauthorized
+      }
+    }
+  }
+
+  // delete indeces
+  it should {
+    "return an HTTP code 200 when deleting an index" in {
+      Delete(s"/index_getjenny_english_0/index_management") ~> addCredentials(testAdminCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        val response = responseAs[IndexManagementResponse]
+      }
+    }
+  }
+
+  it should {
+    "return an HTTP code 200 when deleting an existing system index" in {
+      Delete(s"/system_index_management") ~> addCredentials(testAdminCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        val response = responseAs[IndexManagementResponse]
+      }
+    }
+  }
 
 }
